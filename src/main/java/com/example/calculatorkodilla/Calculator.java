062f5a803bd0c72c2b5cc0e0d2b2c420b74d56d8
Joanna Komoszewska <joanna.komoszewska@gmail.com>
2022-05-03 13:13:06 +0200

    First Commit

--- src/main/java/com/example/calculatorkodilla/Calculator.java
+++ src/main/java/com/example/calculatorkodilla/Calculator.java
@@ -1,4 +1,20 @@
 package com.example.calculatorkodilla;
 
 public class Calculator {
+    public int addAToB(int a, int b){
+        return a + b;
+    }
+    public int subtractAFromB(int a, int b) {
+        return b - a;
+    }
+    public void someMethod(){
+        System.out.println("It works!");
+    }
+    public static void main (String[]args){
+        Calculator calculator1 = new Calculator();
+        calculator1.someMethod();
+        int result = calculator1.addAToB(5, 12);
+        int result1 = calculator1.subtractAFromB(5, 12);
+        System.out.println(result + "and " + result1);
+    }
 }
